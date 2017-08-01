824a1cabad08ecb0c6be457a75b2c90f6485dfcb
Rob McKenzie <mckenro@gmail.com>
2017-08-01 13:40:38 -0700

    quick edit

---
+++ src/app/api-keys.ts
@@ -0,0 +1,7 @@
+export var masterFirebaseConfig = {
+    apiKey: 'AIzaSyA2Tu7cHrggeD2X4C7jG24dSaFxL-iYMpE',
+    authDomain: 'surf-group.firebaseapp.com',
+    databaseURL: 'https://surf-group.firebaseio.com',
+    storageBucket: 'surf-group.appspot.com',
+    messagingSenderId: '799896385183'
+  };
